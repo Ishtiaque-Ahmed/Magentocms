08650129d8fe97ff445c6265575963bbae6e9095
Ishtiaque <babulmalaka@gmail.com>
2018-04-05 19:33:26 +0600

    git

--- app/code/local/HarrisWebworks/Blogpost/Block/Pagination.php
+++ app/code/local/HarrisWebworks/Blogpost/Block/Pagination.php
@@ -14,13 +14,14 @@ class HarrisWebworks_Blogpost_Block_Pagination extends Mage_Core_Block_Template
         //this is toolbar we created in the previous step
         $pager = $this->getLayout()->createBlock('page/html_pager', 'custom.pager');
         //get collection of your model
-        $collection = Mage::getModel('blogpost/blogpost')->getCollection();
+       // $collection = Mage::getModel('blogpost/blogpost')->getCollection();
         $pager->setAvailableLimit(array(2=>2,5=>5,10=>10,20=>20,'all'=>'all'));
+        $this->setLimit($pager->getLimit());
         //this is where you set what options would you like to  have for sorting your grid. Key is your column in your database, value is just value that will be shown in template
        // $toolbar->setAvailableOrders(array('created_at'=> 'Created Time','id'=>'ID'));
         //$toolbar->setDefaultOrder('id');
         //$toolbar->setDefaultDirection("asc");
-        $pager->setCollection($collection);
+        $pager->setCollection($this->getCollection());
         $this->setChild('toolbar', $pager);
         $this->getCollection()->load();
         return $this;
